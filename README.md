366debc9c1edae5a46d0df683352c6ec331812de
Ucan Alpuche Ernesto Javier <23470319@campeche.tecnm.mx>
2025-10-17 14:24:22 -0600

    Version de sincronizacion explicita

---
+++ README.md
@@ -0,0 +1,1 @@
+ContadorManual es una aplicación .NET MAUI la cual permite contar cualquier cosaLayouts muestra ejemplo de uso de los layouts de .NET MAUI
---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCalcularButtonClicked(object sender, EventArgs e)
+        {
+            decimal importeConsumo = Convert.ToDecimal(ConsumoEntry.Text);
+            decimal npersonas = Convert.ToDecimal(PersonasEntry.Text);
+            decimal propina = Convert.ToDecimal(PropinasEntry.Text);
+
+            decimal importedelapropina = ((importeConsumo * propina) / 100);
+            decimal importetotal = importedelapropina + importeConsumo;
+            decimal propinaporpersona = importedelapropina / npersonas;
+            decimal totalporpersona = propinaporpersona / npersonas;
+
+            PropinaLabel.Text = importedelapropina.ToString();
+            TotalLabel.Text = importetotal.ToString();
+            PersonaLabel.Text = propinaporpersona.ToString();
+            PersonaTotalLabel.Text = importetotal.ToString();
+
+        }
+
+        private void OnLimpiarButtonClicked(object sender, EventArgs e)
+        {
+            PropinaLabel.Text = string.Empty;
+            TotalLabel.Text = string.Empty;
+            PersonaLabel.Text = string.Empty;
+            PersonaTotalLabel.Text = string.Empty;
+
+            ConsumoEntry.Text = string.Empty;
+            PersonasEntry.Text = string.Empty;
+            PropinasEntry.Text = string.Empty;
+        }
+    }
+}
